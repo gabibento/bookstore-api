1a7ec739f8393041276fbbaf634e7f57e5588649
Gabriella <gabriella.mabento@gmail.com>
2025-05-02 11:24:24 -0300

    feat: update JwtAuthenticationFilter

--- bookstore/src/main/java/com/api/bookstore/config/JwtAuthenticationFilter.java
+++ bookstore/src/main/java/com/api/bookstore/config/JwtAuthenticationFilter.java
@@ -2,10 +2,14 @@ package com.api.bookstore.config;
 
 import com.api.bookstore.repositories.UserRepository;
 import com.api.bookstore.services.JwtService;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -27,7 +31,21 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             return;
         }
         String token = authHeader.replace("Bearer ", "");
-        String username = jwtService.getUsernameFromToken(token);
 
+        try{
+            String username = jwtService.getUsernameFromToken(token);
+
+            if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+                var user = userRepository.findByUsername(username)
+                        .orElseThrow(() -> new RuntimeException("User not found"));
+
+                var authToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }catch (JwtException e) {
+            System.out.println("Token inválido: " + e.getMessage());
+        }
+        filterChain.doFilter(request, response);
     }
 }
